b11841313892ca4a61b446576f07139e14bbafcb
rickardkamel <rickardkamel@gmail.com>
2017-06-27 07:59:42 +0200

    added Header from react mdl to navigate easier.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-          {this.props.children}
+      <Header/>
+        {this.props.children}
       </div>
     );
   }
