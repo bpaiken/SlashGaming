fea0507b5cc357e94da18b4d12893a038671ba84
Brian Aiken <bpaiken570@gmail.com>
2017-08-29 17:20:12 -0400

    Add user auth actions

---
+++ src/actions/user_auth_actions.js
@@ -0,0 +1,36 @@
+import * as APIUtil from '../util/user_auth_util'
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+
+export const signinUser = user => dispatch => {
+  return APIUtil.signinUser(user)
+  .then(currentUser => dispatch(receiveCurrentUser(currentUser)),
+    error => dispatch(recieveErrors(error)))
+}
+
+export const signupUser = user => dispatch => {
+  return APIUtil.signupUser(user)
+  .then(currentUser => dispatch(receiveCurrentUser(currentUser)),
+    error => dispatch(recieveErrors(error)))
+}
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser,
+  }
+}
+
+export const recieveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  }
+}
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
+  }
+}
