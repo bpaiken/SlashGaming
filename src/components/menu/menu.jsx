49df1fe367eefb9da81c52c04d3e7794c5068557
Brian Aiken <bpaiken570@gmail.com>
2017-09-10 18:48:52 -0400

    Added routes to menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,43 +1,61 @@
 import React from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 import './menu.css'
 
 class SlashMenu extends React.Component {
   state = { activeItem: 'account' }
   
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name, pathName }) => {
+    this.props.history.push(`${pathName}`)
+    this.setState({ activeItem: name })
+    }
 
+  // TODO: Use active links or hookup to display slice of state
   render() {
     const { activeItem } = this.state || {}
     return (
         <Menu vertical inverted fixed="left">
           <Menu.Item>
             <Menu.Header>Account</Menu.Header>
+            
             <Menu.Menu>
-            <Menu.Item name='profile' active={activeItem === 'profile'} onClick={this.handleItemClick}>
-              <Icon name='calendar' /> Profile
-            </Menu.Item>
-              <Menu.Item name='verified-characters' active={activeItem === 'verified-characters'} onClick={this.handleItemClick}>
-              <Icon name='users' /> Verified characters
-            </Menu.Item>
-              <Menu.Item name='verify-character' active={activeItem === 'verify-character'} onClick={this.handleItemClick}>
-              <Icon name='add user' /> Verify character
-            </Menu.Item>
-              <Menu.Item name='settings' active={activeItem === 'settings'} onClick={this.handleItemClick}>
-              <Icon name='setting' /> Settings
-            </Menu.Item>
+              <Menu.Item name='profile' pathName='/dashboard'
+                active={activeItem === 'profile'} onClick={this.handleItemClick}>
+                <Icon name='calendar' /> Profile
+              </Menu.Item>
+
+              <Menu.Item name='verified-characters' pathName='/'
+                active={activeItem === 'verified-characters'} onClick={this.handleItemClick}>
+                <Icon name='users' /> Verified characters
+              </Menu.Item>
+
+              <Menu.Item name='verify-character' pathName='/user/verify-character' 
+                active={activeItem === 'verify-character'} onClick={this.handleItemClick}>
+                <Icon name='add user' /> Verify character
+              </Menu.Item>
+
+              <Menu.Item name='settings' pathName='/'
+               active={activeItem === 'settings'} onClick={this.handleItemClick}>
+                <Icon name='setting' /> Settings
+              </Menu.Item>
             </Menu.Menu>
           </Menu.Item>
+          
           <Menu.Item>
             <Menu.Header>Events</Menu.Header>
+
             <Menu.Menu>
-              <Menu.Item name='upcoming' active={activeItem === 'upcoming'} onClick={this.handleItemClick}>
-              <Icon name='bell' /> Upcoming events
-            </Menu.Item>
-              <Menu.Item name='closed' active={activeItem === 'closed'} onClick={this.handleItemClick}>
-              <Icon name='checkmark' /> Closed events
-            </Menu.Item>
+              <Menu.Item name='upcoming' pathName='/' 
+                active={activeItem === 'upcoming'} onClick={this.handleItemClick}>
+                <Icon name='bell' /> Upcoming events
+              </Menu.Item>
+              
+              <Menu.Item name='closed' pathName='/'
+                active={activeItem === 'closed'} onClick={this.handleItemClick}>
+                <Icon name='checkmark' /> Closed events
+              </Menu.Item>
             </Menu.Menu>
           </Menu.Item>
         </Menu>
@@ -45,7 +63,9 @@ class SlashMenu extends React.Component {
   }
 }
 
+///// CONTAINER /////
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 const mapStateToProps = ({ display }) => {
   return {
@@ -59,7 +79,7 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps, 
   mapDispatchToProps)
-  (SlashMenu)
+  (SlashMenu))
