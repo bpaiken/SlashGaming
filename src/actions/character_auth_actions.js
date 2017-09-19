63768f402c82013a164b0bf7ab7aaec09f3b3ca4
Brian <bpaiken>
2017-09-19 10:33:16 -0400

    updating character auth actions with errors

--- src/actions/character_auth_actions.js
+++ src/actions/character_auth_actions.js
@@ -1,6 +1,7 @@
 import * as APIUtil from '../util/character_auth_util'
 export const RECEIVE_VERIFIED_CHARACTER = 'RECEIVE_VERIFIED_CHARACTER'
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
+export const RECEIVE_RESPONSE_ERROR = 'RECEIVE_RESPONSE_ERROR'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
 
 const recieveVerifiedCharacter = ({ character }) => {
   return {
@@ -9,21 +10,27 @@ const recieveVerifiedCharacter = ({ character }) => {
   }
 }
 
-const receiveErrors = errors => {
+const receiveErrors = ({ status }) => {
   return {
-    type: RECEIVE_ERRORS,
-    errors,
+    type: RECEIVE_RESPONSE_ERROR,
+    status,
+  }
+}
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
   }
 }
 
 export const verifyCharacter = character => dispatch => {
   return APIUtil.verifyCharacter(character)
   .then(response => {console.log(response)},
-  error => dispatch(receiveErrors(error)))
+  error => dispatch(receiveErrors(error.response)))
 }
 
 export const verifyCode = code => dispatch => {
   return APIUtil.verifyCode(code)
   .then(response => dispatch(recieveVerifiedCharacter(response.data)),
-  error => dispatch(receiveErrors(error)))
+  error => dispatch(receiveErrors(error.response)))
 }
