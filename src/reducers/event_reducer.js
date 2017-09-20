c982426f92f194025fc7319b709e2d0d6ab27c85
Calvin Lee <clee421@gmail.com>
2017-09-20 14:41:25 -0400

    hard code initial state for events

--- src/reducers/event_reducer.js
+++ src/reducers/event_reducer.js
@@ -1,6 +1,38 @@
 // import merge from 'lodash/merge'
 
-const initialState = {}
+// Below is for testing only
+// Format
+// {
+// 	"name": "Some new event",
+// 	"start": "2017-09-25T10:12:25.002Z",
+// 	"end": "2017-10-29T10:12:25.002Z"
+// }
+const initialState = {
+  1: {
+    id: 1,
+    name: 'Amazing Event 1',
+    start: '2017-09-11T10:12:25.002Z',
+    end: '2017-10-15T10:12:25.002Z',
+  },
+  2: {
+    id: 2,
+    name: 'Amazing Event 2',
+    start: '2017-09-16T10:12:25.002Z',
+    end: '2017-10-18T10:12:25.002Z',
+  },
+  3: {
+    id: 3,
+    name: 'Amazing Event 3',
+    start: '2017-09-19T10:12:25.002Z',
+    end: '2017-10-21T10:12:25.002Z',
+  },
+  4: {
+    id: 4,
+    name: 'Amazing Event 4',
+    start: '2017-09-25T10:12:25.002Z',
+    end: '2017-10-29T10:12:25.002Z',
+  }
+}
 
 const eventReducer = (state = initialState, action) => {
   Object.freeze(state)
