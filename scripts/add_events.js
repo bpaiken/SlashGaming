79e9f91f8078e0956284b816863b11e82d6e741f
Brian Aiken <bpaiken570@gmail.com>
2017-09-23 16:12:42 -0400

    update add_event script

--- scripts/add_events.js
+++ scripts/add_events.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // Adding random events using node
 const axios = require('axios') 
 
@@ -12,18 +14,32 @@ const addEvents = (events, token) => {
     })
 }
 
-// Can't add status
-let event = {
-	name: "amazing super awesome event",
-	start: "2017-08-25T10:12:25.002Z",
-  end: "2017-08-29T10:12:25.002Z"
+const events = {
+  event_one: {
+    name: "first event",
+    start: "2017-10-14T10:12:25.002Z",
+    end: "2017-10-15T10:12:25.002Z"
+  },
+  event_two: {
+    name: "second event",
+    start: "2017-10-17T10:12:25.002Z",
+    end: "2017-10-19T10:12:25.002Z"
+  },
+  event_three: {
+    name: "third event",
+    start: "2017-10-20T10:12:25.002Z",
+    end: "2017-10-21T10:12:25.002Z"
+  },
+  event_four: {
+    name: "fourth event",
+    start: "2017-10-22T10:12:25.002Z",
+    end: "2017-10-24T10:12:25.002Z"
+  }
 }
-
-// hard coding tokens
-let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhY2Nlc3MiLCJleHAiOjE1MDYxMjg1NTgsImlhdCI6MTUwNjA0MjE1OCwiaXNzIjoic2xhc2hnYW1pbmcubmV0IiwianRpIjoiZDNhNTk0M2ItMGRhZi00OTNhLWEwMWYtNWZiOTNhMjI0OGVjIiwibmJmIjoxNTA2MDQyMTU4LCJyb2xlIjoidXNlciIsInN1YiI6ImNhbHZpbiIsInVzZXJfaWQiOjF9.c4VZZjuWPMp7ZsRRyJmwwMWqArUuYqFRHdr8TL5KdfM"
-
-// still have to go into database and set some events to closed
-addEvents(event, token).then(
-  resp => console.log(resp.data),
-  err => console.log(err)
-)
+  
+  // hard coding tokens
+  let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhY2Nlc3MiLCJleHAiOjE1MDYyODM5MTEsImlhdCI6MTUwNjE5NzUxMSwiaXNzIjoic2xhc2hnYW1pbmcubmV0IiwianRpIjoiMDJmOTg0NjgtYjczMy00YThkLTgzMDUtYzMwMWExZDZmOWE5IiwibmJmIjoxNTA2MTk3NTExLCJyb2xlIjoiYWRtaW4iLCJzdWIiOiJzZWlyaWYiLCJ1c2VyX2lkIjoxfQ.YtkUKmaiQPTWjezChFfa358OBCW2SNlwbQQyu2WnHYw"
+  
+Object.keys(events).forEach(event => {
+  addEvents(event, token)
+})
