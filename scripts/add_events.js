f319ad68298b7fd5bb8f262e2567aa75d251dbeb
Calvin Lee <clee421@gmail.com>
2017-09-23 21:13:53 -0400

    added number of events to add to add_event script

--- scripts/add_events.js
+++ scripts/add_events.js
@@ -9,7 +9,7 @@ const login = (username, password) => {
   return axios.post(`${API_URL}/authenticate/user`, {username, password})
 }
 
-const addEvents = (events, token) => {
+const addEvent = (events, token) => {
   return axios.post(`${API_URL}/events`, events,{
     headers: 
       { 
@@ -18,43 +18,47 @@ const addEvents = (events, token) => {
     })
 }
 
-const events = {
-  event_one: {
-    name: "first event",
-    start: "2017-10-14T10:12:25.002Z",
-    end: "2017-10-15T10:12:25.002Z"
-  },
-  event_two: {
-    name: "second event",
-    start: "2017-10-17T10:12:25.002Z",
-    end: "2017-10-19T10:12:25.002Z"
-  },
-  event_three: {
-    name: "third event",
-    start: "2017-10-20T10:12:25.002Z",
-    end: "2017-10-21T10:12:25.002Z"
-  },
-  event_four: {
-    name: "fourth event",
-    start: "2017-10-22T10:12:25.002Z",
-    end: "2017-10-24T10:12:25.002Z"
+const adjectives = [
+  "super",
+  "awesome",
+  "crazy",
+  "amazing",
+  "sick"
+]
+
+const createMultEvents = (num, token) => {
+  for (let i = 1; i <= num; i++) {
+    let adj1 = adjectives[Math.floor(Math.random() * adjectives.length)]
+    let adj2 = adjectives[Math.floor(Math.random() * adjectives.length)]
+    let adj3 = adjectives[Math.floor(Math.random() * adjectives.length)]
+  
+    // using dates 10 to 20 because 1-9 causes problems and i'm
+    // too lazy to pad it
+    let date = Math.floor(Math.random() * 10) + 10
+  
+    addEvent({
+      name: `${adj1} ${adj2} ${adj3} event`,
+      start: `2017-10-${date}T10:12:25.002Z`,
+      end: `2017-10-${date + Math.floor(Math.random() * 5)}T10:25:25.002Z`
+    }, token).then(
+      resp => {
+        console.log(`${resp.data.name} created`)
+      },
+      err => console.log(err)
+    )
   }
 }
-  
-// hard coding tokens
-// let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhY2Nlc3MiLCJleHAiOjE1MDYyODM5MTEsImlhdCI6MTUwNjE5NzUxMSwiaXNzIjoic2xhc2hnYW1pbmcubmV0IiwianRpIjoiMDJmOTg0NjgtYjczMy00YThkLTgzMDUtYzMwMWExZDZmOWE5IiwibmJmIjoxNTA2MTk3NTExLCJyb2xlIjoiYWRtaW4iLCJzdWIiOiJzZWlyaWYiLCJ1c2VyX2lkIjoxfQ.YtkUKmaiQPTWjezChFfa358OBCW2SNlwbQQyu2WnHYw"
 
 // TODO: extra arguments for options
 // username and password is taken from commandline when running script
-// $ node ./scripts/add_events.js calvin password
+// $ node ./scripts/add_events.js calvin password 10
 let user = process.argv[2]
 let pass = process.argv[3]
+let numEvents = Number(process.argv[4])
 
 login(user, pass).then(
   resp => {
-    Object.keys(events).forEach(event => {
-      addEvents(events[event], resp.data.token)
-    })
+    createMultEvents(numEvents, resp.data.token)
   },
   err => console.log(err)
 )
