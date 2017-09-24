cc23c312df76a3e293ee3f6178a096c4bad0ed67
Brian <bpaiken>
2017-09-24 11:53:13 -0400

    Add skeleton event component

---
+++ src/components/event.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class Event extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    
+    return (
+
+    )
+  }
+}
+
+///// CONTAINER /////
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dipatch) => {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Event)
